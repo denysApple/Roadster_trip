3c311789e9d33380ee0d053ab804692fbf9cf4e5
Denys "Den4ikLvivUA" Havryliak <35520974+Den4ikLvivUA@users.noreply.github.com>
2018-10-21 09:56:18 +0300

    Initial commit

---
+++ Assets/CameraScripts.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScripts : MonoBehaviour {
+	public int speed = 10;//Наша скорость
+
+	void Update () {
+	if (Input.GetKey (KeyCode.W)) {
+			gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+				}
+		if (Input.GetKey (KeyCode.S)) {
+			gameObject.transform.position -= gameObject.transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			gameObject.transform.position -= gameObject.transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			gameObject.transform.position += gameObject.transform.right * speed * Time.deltaTime;
+		}
+	}
+}
